10734ca0b9eaa1a24068be70e7e31967895e0bd7
Sara Tamer <S320022@ese.gov.ae>
2023-06-24 12:22:56 +0200

    Initial Commit

---
+++ Get_test/Get_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  Get_testApp.swift
+//  Get_test
+//
+//  Created by Sara Tamer on 24/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct Get_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
